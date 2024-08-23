bc668f82adc826e936876c68593b4ee1bf084913
khomah8 <59750654+khomah8@users.noreply.github.com>
2024-08-23 16:42:00 +0300

    Update daily-earn.sh

--- daily-earn.sh
+++ daily-earn.sh
@@ -4,8 +4,11 @@ find _folder-path_ -type f -name ‘*.log’ -exec grep -l ERROR {} \;
 #similar search 
 grep -H "ERROR" _folder-path_*.log | cut -d: -f1
 
+## free
+
+
 ## tar 
-/home/hpstandard 15:11 tar -tvf tar55.tar
+$ tar -tvf tar55.tar
 -rw-r--r-- root/root       208 2023-12-28 14:59 tar--t
 -rw-r--r-- root/root        37 2023-12-28 14:57 tarr--t
 /home/hpstandard 15:13 tar --delete -f tar55.tar tar--t
